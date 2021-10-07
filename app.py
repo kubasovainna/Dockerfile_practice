92cf5a7e90456d6a27c432ba08bf64cb6424bdba
kubasovainna <kubasovainna@mail.ru>
2021-10-07 16:05:58 +0300

    Counter added

--- app.py
+++ app.py
@@ -3,13 +3,25 @@ import os
 import socket
 
 app = Flask(__name__)
+c = 0
 
 @app.route("/")
+def current_value_c():
+    return f"Current counter value: {c}"
+
+@app.route("/stat")
+def increment_с():
+    global c
+    msg = f"Current counter value: {c}"
+    c += 1
+    return msg + f"\nThe counter value has been incremented."
+
+@app.route("/about")
 def hello():
     
-    html = "<h3>Hello!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>"
-    return html.format(name=os.getenv("NAME", "Inna"), hostname=socket.gethostname())
+    html = "<h1>Greetings from Inna!</h1>" \
+           #"<b>Hostname:</b> {hostname}<br/>"
+    return html.format(name=os.getenv("NAME", "World"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
